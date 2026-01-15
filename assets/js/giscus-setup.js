2b7e2ee679c6e1ff3ae038494cc08f23edccf4cb
hongyehu <50563225+hongyehu@users.noreply.github.com>
2026-01-15 01:47:36 +0000

    Deploying to gh-pages from @ hongyehu/hongyehu.github.io@3bfd6a3cc064743941ff7f56b5497315e00278bb 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "hongyehu/hongyehu",
+    "data-repo": "hongyehu/hongyehu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
